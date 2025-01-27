d8245f11cb4ded5e3f2eb20abe0ce59397670e0f
Jaka <jakanggara@gmail.com>
2025-01-28 01:25:16 +0700

    chore: implement api

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"log"
+	"skill_test/api"
+	"skill_test/data"
 	"skill_test/models"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -16,4 +19,12 @@ func main() {
 
 	// Automatically migrate your schema
 	db.AutoMigrate(&models.DbSource{}, &models.DbDestination{})
+
+	totalData := 500
+	data.Seed(db, uint(totalData))
+
+	r := gin.Default()
+	api.InitRoutes(r, db)
+
+	r.Run(":8001")
 }
